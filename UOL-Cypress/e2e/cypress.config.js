43b75e54803f1f3d3cd68d12bb500f42151cdd08
KevinAlvesDev <kevinalveswp@gmail.com>
2025-08-07 08:34:52 -0300

    Atualizado

--- UOL-Cypress/e2e/cypress.config.js
+++ UOL-Cypress/e2e/cypress.config.js
@@ -3,10 +3,9 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://www.uol.com.br',
-    pageLoadTimeout: 240000, // espera até 4 minutos para carregar a página
-    defaultCommandTimeout: 20000, // tempo padrão para comandos como cy.get()
+    pageLoadTimeout: 240000,
+    defaultCommandTimeout: 20000,
     setupNodeEvents(on, config) {
-      // eventos personalizados, se precisar
     },
   },
 })
--- UOL-Cypress/e2e/uol_termos.cy.js
+++ UOL-Cypress/e2e/uol_termos.cy.js
@@ -1,14 +1,11 @@
 describe('Validação do link de normas no UOL', () => {
   beforeEach(() => {
-    // Ignora erros externos que podem quebrar o teste
     Cypress.on('uncaught:exception', () => false)
   })
 
   it('Acessa o UOL e valida que o link de normas apareceu', () => {
-    // Acessa o site UOL com timeout estendido para carregar tudo
     cy.visit('https://www.uol.com.br/', { timeout: 120000 })
 
-    // Aguarda até que o link específico apareça e esteja visível
     cy.get('a[href="https://sobreuol.noticias.uol.com.br/normas-de-seguranca-e-privacidade.html"]', { timeout: 60000 })
       .should('be.visible')
   })
@@ -23,14 +20,11 @@ describe('Validação do elemento <strong>Atualização:</strong> na página de 
     cy.origin('https://sobreuol.noticias.uol.com.br', () => {
       cy.visit('/normas-de-seguranca-e-privacidade', { timeout: 120000 })
 
-      cy.wait(30000) // espera 30 segundos
+      cy.wait(30000)
 
-      // Scroll até o parágrafo com o estilo e que contém <strong>Atualização:</strong>
       cy.get('p[style="margin-left:0cm;text-align:justify;"]')
         .contains('Atualização:')
         .scrollIntoView({ duration: 1000 })
-
-      // Valida que o <strong>Atualização:</strong> está visível
       cy.get('strong').contains('Atualização:').should('be.visible')
     })
   })
